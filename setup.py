3be72588bcb1e7312103ad4e40a671b1dd4d5539
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-02 17:56:18 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def find_version(*file_paths):
 
 __version__ = find_version("plot_keras_history", "__version__.py")
 
-test_deps = ['pytest', 'pytest-cov', 'coveralls', 'validate_version_code']
+test_deps = ['pytest', 'pytest-cov', 'coveralls', 'validate_version_code', 'codacy-coverage']
 
 extras = {
     'test': test_deps,
