ab159d5e9d8de5c113a731f77e7b7549e77830a4
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-03 15:10:45 +0200

    Introduced customization callback

--- plot_keras_history/__version__.py
+++ plot_keras_history/__version__.py
@@ -1,2 +1,2 @@
 """Current version of package plot_keras_history"""
-__version__ = "1.1.9"
+__version__ = "1.1.10"
--- plot_keras_history/plot_keras_history.py
+++ plot_keras_history/plot_keras_history.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from typing import List, Dict, Union, Tuple
+from typing import List, Dict, Union, Tuple, Callable
 import math
 import numpy as np
 import pandas as pd
@@ -24,18 +24,17 @@ def get_figsize(n: int, graphs_per_row: int)->Tuple[int, int]:
     return min(n, graphs_per_row), math.ceil(n/graphs_per_row)
 
 
-def plot_history(history: Union[Dict[str, List[float]], pd.DataFrame], interpolate: bool = False, side: float = 5, graphs_per_row: int = 4, x_label: str = None):
+def plot_history(history: Union[Dict[str, List[float]], pd.DataFrame], interpolate: bool = False, side: float = 5, graphs_per_row: int = 4, customization_callback: Callable = None):
     """Plot given training history.
         history:Dict[str, List[float]], the history to plot.
         interpolate:bool=False, whetever to reduce the graphs noise.
         side:int=5, the side of every sub-graph.
         graphs_per_row:int=4, number of graphs per row.
-        x_label:str=None, label for the X axis.
+        customization_callback:Callable=None, callback for customising axis.
     """
     if not isinstance(history, pd.DataFrame):
         history = pd.DataFrame(history)
-    if x_label is None:
-        x_label = "Epochs" if history.index.name is None else history.index.name
+    x_label = "Epochs" if history.index.name is None else history.index.name
     metrics = [m for m in history if not m.startswith("val_")]
     n = len(metrics)
     w, h = get_figsize(n, graphs_per_row)
@@ -56,6 +55,8 @@ def plot_history(history: Union[Dict[str, List[float]], pd.DataFrame], interpola
         axis.legend()
         if history.shape[0] <= 4:
             axis.set_xticks(range(history.shape[0]))
+        if customization_callback is not None:
+            customization_callback(axis)
 
     for axis in flat_axes:
         axis.axis("off")
