5c09b496a0989f3a5e573763c77e41919eebab1d
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-27 08:56:12 +0100

    Removed useless files from repo

--- build/lib/plot_keras_history/__init__.py
+++
@@ -1,4 +0,0 @@
-from .plot_keras_history import plot_history
-from .utils import chain_histories
-
-__all__ = ["plot_history", "chain_histories"]
--- build/lib/plot_keras_history/__version__.py
+++
@@ -1,2 +0,0 @@
-"""Current version of package."""
-__version__ = "1.0.2"
