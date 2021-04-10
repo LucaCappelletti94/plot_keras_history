9df328ae01f673a0a7d85b64176dfc1208e5bce7
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2021-04-10 15:05:00 +0200

    Extended test dependencies

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def find_version(*file_paths):
 
 __version__ = find_version("plot_keras_history", "__version__.py")
 
-test_deps = ['pytest', 'pytest-cov', 'coveralls', 'validate_version_code', 'codacy-coverage', "compress_json"]
+test_deps = ['pytest', 'pytest-cov', 'coveralls', 'validate_version_code', 'codacy-coverage', "compress_json", "tensorflow"]
 
 extras = {
     'test': test_deps,
