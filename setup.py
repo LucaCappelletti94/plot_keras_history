6719e72ca33cf5902ed79572b84700c7834f0075
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2022-04-14 18:14:41 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
         "matplotlib",
         "pandas",
         "scipy",
-        "sanitize_ml_labels>=1.0.28"
+        "sanitize_ml_labels>=1.0.31"
     ],
     tests_require=test_deps,
     extras_require=extras,
